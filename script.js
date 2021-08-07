93b20da705fcebd9939999cfb67dfd5063189932
user123 <mo7amedfares321@gmail.com>
2021-08-07 19:46:16 +0200

    aeae

--- script.js
+++ script.js
@@ -4,18 +4,47 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+
+
+
+const pg = require("pg").Client
+const client = new pg({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+
+
 app.get("/",(req,resp)=>{
+
+    app.get("/",(req,resp)=>{
+    client.connect()
+    .then(()=>client.query("select * from users"))
+    .then(data=>{resp.json(data)})
+    .finally(()=>client.end())
+
     
-    resp.json("dose it even work")
-    // resp.send("this works yey")
-})
-console.log("dose it work")
+
+})})
 app.listen(process.env.PORT || 3000)
 
+   
+  
+  
+  
 
 
 
 
+
+
+
+// client.connect()
+// .then(()=>client.query("select * from users"))
+// .then(data=>{console.log(data)})
+// .finally(()=>client.end())
 
 
 
