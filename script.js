52fc4efde4aad7de98bc5e3ce6eb3b4373d1cd25
user123 <mo7amedfares321@gmail.com>
2021-08-08 15:28:19 +0200

    asdas

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const pg = require("pg")
 
 
 app.get("/",(req,resp)=>{
-  const client = new pg({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: true
-    }
-  })
-  client.connect();
-  client.query('select * from users', (err, result) => {
-      console.log('err', err)
-      console.log('result', result)
-      return resp.status(200).json(result.rows)
-  })
+  // const client = new pg({
+  //   connectionString: process.env.DATABASE_URL,
+  //   ssl: {
+  //     rejectUnauthorized: true
+  //   }
+  // })
+  // client.connect();
+  // client.query('select * from users', (err, result) => {
+  //     console.log('err', err)
+  //     console.log('result', result)
+  //     return resp.status(200).json(result.rows)
+  // })
   resp.json("asdasdas")
 
 })
