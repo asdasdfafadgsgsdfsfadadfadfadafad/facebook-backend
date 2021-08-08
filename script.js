97484e559ea617c318e9a314a3637e7110dc80f3
user123 <mo7amedfares321@gmail.com>
2021-08-08 09:13:01 +0200

    aaazz

--- script.js
+++ script.js
@@ -24,18 +24,18 @@ const cors = require("cors")
 const app = express()
 app.use(cors())
 app.use(express.json())
-
 // const knex = require('knex')({
 //   client:"pg",
 //   connection:{connectionString:process.env.DATABASE_URL,ssl:true}
 // });
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; //add this line is you have ssl:true
 const knex = knex({
   client: 'pg',
   connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized },
-  }
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
+}); 
 app.get("/",async (req,resp)=>{
     const data = await knex.select("*").from("users")
     resp.json(data)
