8caf5743553891a155b21681c4dc44468d934d38
user123 <mo7amedfares321@gmail.com>
2021-08-08 08:41:03 +0200

    asdsaasdsadasa

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
-const express = require("express");
+// const express = require("express");
+// const cors = require("cors")
+// const app = express()
+// app.use(express.json())
+// app.use(cors())
+// const pg = require("pg").Client;
+// const { json } = require("express");
+// const client = new pg({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   })
+// app.get("/",async (req,resp)=>{
+//     await client.connect()
+//     const data = await client.query("select * from users")
+//     await client.end()
+//     resp.json(data)
+// })
+// app.listen(process.env.PORT || 3000)
+
+const express = require("express")
 const cors = require("cors")
 const app = express()
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
-
-
-
-
-const pg = require("pg").Client;
-const { json } = require("express");
-const client = new pg({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-
-
+const knex = require('knex')({
+  client:"pg",
+  connection:{connectionString:process.env.DATABASE_URL,ssl:true}
+});
 app.get("/",async (req,resp)=>{
-
-
-    // client.connect()
-    // .then(()=>client.query("select * from users"))
-    // .then(data=>{
-    //     client.end()
-    //     resp.json(data)
-    // })
-    // .finally(()=>client.end())
-    await client.connect()
-    const data = await client.query("select * from users")
-    await client.end()
+    const data = await knex.select("*").from("users")
     resp.json(data)
-    // .then(()=>client.query("select * from users"))
-    // .then(data=>{resp.json(data)})
-    // .finally(()=>client.end())
-    
-
 })
 app.listen(process.env.PORT || 3000)
-
    
   
   
@@ -50,11 +44,6 @@ app.listen(process.env.PORT || 3000)
 
 
 
-
-// client.connect()
-// .then(()=>client.query("select * from users"))
-// .then(data=>{console.log(data)})
-// .finally(()=>client.end())
 
 
 
